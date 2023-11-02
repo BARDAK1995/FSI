baca6c8f6d43f4c1afd97eda4023da345198fc2f
Mert Senkardesler <mertsenkardesler@yahoo.com.tr>
2023-11-02 00:19:00 -0500

    newstart

---
+++ psd.py
@@ -0,0 +1,192 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import signal
+import numpy as np
+# Read the file into a DataFrame, assuming space-separated values and no header
+caseName = "CASE1_half"
+# caseName = "CASE2"
+point = 3
+caseName = "oscVel_pm"
+# caseName = "oscilatingPM50"
+
+file_path = f"./PM_BC_1/{caseName}/Point{str(point)}.dat"
+file_path2 = f"./PM_BC_1/ref/Point{str(point)}.dat"
+
+# file_path2 = f"./PM_BC_1/{caseName}/Point{str(point+5)}.dat"
+# file_path2 = f"./Phonic_state_MS3/{caseName}/Point{str(point)}.dat"
+# file_path = "8Hugejet.dat"
+
+df = pd.read_csv(file_path, delim_whitespace=True, header=None)
+df2 = pd.read_csv(file_path2, delim_whitespace=True, header=None)
+# Extract the first column (time step) and the 10th column (data of interest)
+
+def plotPSD(data):
+    tau = 5.0e-9   
+    time_step = np.array(data[0]) * tau
+    data_of_interest = np.array(data[13])
+    cutoff = len(data_of_interest)//2
+    cutoffTime = cutoff * tau
+    actual_data = data_of_interest[cutoff:]
+    cutoff_timesteps = time_step[cutoff:]
+
+    # # Plot the data
+    # plt.figure(figsize=(12, 6))
+    # plt.plot(cutoff_timesteps, actual_data, marker='o', linestyle='-')
+    # plt.xlabel('Time (s))')
+    # plt.ylabel('Pressure (Pa)')
+    # plt.title('Pressure vs Time')
+    # # plt.grid(True)
+    # plt.show()
+
+    # # # Detrend the data to make it stationary
+    detrended_data = signal.detrend(actual_data)
+    # plt.figure(figsize=(12, 6))
+    # plt.plot(cutoff_timesteps, detrended_data, marker='o', linestyle='-')
+    # plt.xlabel('Time (s))')
+    # plt.ylabel('Pressure Perturbation (Pa)')
+    # plt.title('Unsteady Pressure')
+    # # plt.grid(True)
+    # plt.show()
+    detrended_data_array = np.array(detrended_data)
+    # Calculate the Power Spectral Density using Fast Fourier Transform
+    samplingFreq = 1/tau
+    frequencies, psd_values = signal.welch(detrended_data_array, fs=samplingFreq, nperseg=10030)
+    # Plotting the Power Spectral Density
+    plt.figure(figsize=(12, 6))
+    plt.loglog(frequencies/1000, psd_values)
+    plt.title('Power Spectral Density')
+    plt.xlabel('Frequency [kHz]')
+    plt.ylabel('PSD [V**2/Hz]')
+    plt.ylim([10**-12, 1*10**1]) # setting y-axis range
+
+    plt.grid(True)
+    plt.show()
+
+def plotPSD2(data, data2):
+    tau = 5.0e-9   
+    index = 16 #pressure
+    # index = 10 #xvel
+    # index = 11 #yvel
+    # index = 13 #transtemp
+    # index = 9
+    time_step = np.array(data[0]) * tau
+    time_step2 = np.array(data2[0]) * tau
+    # 10vx 11vy 13t 14trot 15tvib 16p
+    data_of_interest = np.array(data[index])
+    data_of_interest2 = np.array(data2[index])
+
+    cutoff = len(data_of_interest)//6
+    cutoff2 = len(data_of_interest2)//6
+
+    cutoffTime = cutoff * tau
+    cutoffTime2 = cutoff2 * tau
+    
+    actual_data = data_of_interest[cutoff:]
+    actual_data2 = data_of_interest2[cutoff2:]
+
+    cutoff_timesteps = time_step[cutoff:]
+    cutoff_timesteps2 = time_step2[cutoff2:]
+    asd=9*(10**8)*2*(10**12)
+    # Plot the data
+    plt.figure(figsize=(12, 6))
+    # plt.plot(cutoff_timesteps, actual_data, marker='o', linestyle='-')
+    plt.plot(cutoff_timesteps, actual_data,"blue",linewidth=0.3, linestyle='-')
+    plt.xlabel('Time (s))')
+    plt.ylabel('Pressure (pa)')
+    plt.title('Pressure vs Time')
+    # plt.grid(True)
+    plt.show()
+
+    # # # Detrend the data to make it stationary
+    detrended_data = signal.detrend(actual_data)
+    detrended_data2 = signal.detrend(actual_data2)
+    # plt.figure(figsize=(12, 6))
+    # plt.plot(cutoff_timesteps, detrended_data, marker='o', linestyle='-')
+    # plt.xlabel('Time (s))')
+    # plt.ylabel('Pressure Perturbation (Pa)')
+    # plt.title('Unsteady Pressure')
+    # # plt.grid(True)
+    # plt.show()
+    detrended_data_array = np.array(detrended_data)
+    detrended_data_array2 = np.array(detrended_data2)
+    # Calculate the Power Spectral Density using Fast Fourier Transform
+    samplingFreq = 1/tau
+
+    frequencies, psd_values = signal.welch(detrended_data_array, fs=samplingFreq, nperseg=100000)
+    frequencies2, psd_values2 = signal.welch(detrended_data_array2, fs=samplingFreq, nperseg=100000)
+
+    # Plotting the Power Spectral Density
+    plt.figure(figsize=(12, 6))
+    plt.loglog(frequencies/1000, psd_values,"black",linewidth=3, label='probe 3') #label='Perturbed Flow via Pulsed Jet'
+    plt.loglog(frequencies2/1000, psd_values2,linewidth=3, label='reference') #label='No Jet(Reference State)'
+    plt.title('PSD', fontsize=22)
+    plt.xlabel('Frequency [kHz]', fontsize=18)
+    plt.ylabel('PSD [T**2/Hz]', fontsize=18)
+    plt.xlim([5*10**1, 10**4])
+    plt.ylim([2*10**-8, 1*10**-1]) # setting y-axis range
+    plt.legend(fontsize=20)
+    plt.grid(True)
+    plt.xticks(fontsize=15)
+    plt.yticks(fontsize=15)
+    plt.show()
+
+def plotPSD22(data, data2):
+    tau = 5.0e-9   
+    time_step = np.array(data[0]) * tau
+    time_step2 = np.array(data2[0]) * tau
+    # 10vx 11vy 13t 14trot 15tvib 16p
+    data_of_interest = np.array(data[16])
+    data_of_interest2 = np.array(data2[16])
+
+    cutoff = len(data_of_interest)//4
+    cutoff2 = len(data_of_interest2)//4
+
+    cutoffTime = cutoff * tau
+    cutoffTime2 = cutoff2 * tau
+    
+    actual_data = data_of_interest[cutoff:]
+    actual_data2 = data_of_interest2[cutoff2:]
+
+    cutoff_timesteps = time_step[cutoff:]
+    cutoff_timesteps2 = time_step2[cutoff2:]
+
+    # # Plot the data
+    # plt.figure(figsize=(12, 6))
+    # plt.plot(cutoff_timesteps, actual_data, marker='o', linestyle='-')
+    # plt.xlabel('Time (s))')
+    # plt.ylabel('Pressure (Pa)')
+    # plt.title('Pressure vs Time')
+    # # plt.grid(True)
+    # plt.show()
+
+    # # # Detrend the data to make it stationary
+    detrended_data = signal.detrend(actual_data)
+    detrended_data2 = signal.detrend(actual_data2)
+    # plt.figure(figsize=(12, 6))
+    # plt.plot(cutoff_timesteps, detrended_data, marker='o', linestyle='-')
+    # plt.xlabel('Time (s))')
+    # plt.ylabel('Pressure Perturbation (Pa)')
+    # plt.title('Unsteady Pressure')
+    # # plt.grid(True)
+    # plt.show()
+    detrended_data_array = np.array(detrended_data)
+    detrended_data_array2 = np.array(detrended_data2)
+    # Calculate the Power Spectral Density using Fast Fourier Transform
+    samplingFreq = 1/tau
+
+    frequencies, psd_values = signal.welch(detrended_data_array, fs=samplingFreq, nperseg=20000)
+    frequencies2, psd_values2 = signal.welch(detrended_data_array2, fs=samplingFreq, nperseg=20000)
+
+    # Plotting the Power Spectral Density
+    plt.figure(figsize=(12, 6))
+    plt.loglog(frequencies/1000, psd_values,linewidth=2.5, label='Case 1')
+    plt.loglog(frequencies2/1000, psd_values2, label='Case 2 1/3rho 3xSpeed')
+    plt.title('Power Spectral Density')
+    plt.xlabel('Frequency [kHz]')
+    plt.ylabel('PSD [P**2/Hz]')
+    plt.ylim([10**-10, 2*10**-2]) # setting y-axis range
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+# plotPSD(df)
+plotPSD2(df,df2)
---
+++ tecplotCompare.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import numpy as np
+casename = "case1_half"
+casename = "pm40_new"
+
+file_path_main = f'./PM_BC2/{casename}.tec'
+# file_path_main31 = f'./Phonic_state_MS2/{casename}_half_modified.tec'
+# file_path_main32 = f'./Phonic_state_MS2/{casename}_modified.tec'
+file_path_ref = './PM_BC2/ref.tec'
+new_file_path = f'./PM_BC2/{casename}_modified.tec'
+
+# file_path_ref = f'./Phonic_state_MS4/{casename}_half.tec'
+# new_file_path = f'./Phonic_state_MS4/{casename}_modified_UNSTEADY.tec'
+
+# file_path_ref = f'./Phonic_state_MS4/{casename}_otherhalf.tec'
+# new_file_path = f'./Phonic_state_MS4/{casename}_modified_UNSTEADY2.tec'
+
+# file_path_main = f'./Phonic_state_MS4/ref5.tec'
+# file_path_ref = f'./Phonic_state_MS4/ref_half.tec'
+# new_file_path = f'./Phonic_state_MS4/ref_modified_UNSTEADY.tec'
+
+with open(file_path_main, 'r') as mainFile, open(file_path_ref, 'r') as refFile, open(new_file_path, 'w') as modified:
+    # Writing the first and modified second lines to the new file
+    first_line = mainFile.readline().strip()
+    variables_line = mainFile.readline().strip()
+    third_line = mainFile.readline().strip()
+    refFile.readline()
+    refFile.readline()
+    refFile.readline()
+
+    preview_lines = [first_line, variables_line, third_line]
+
+    variables = variables_line.split('"')[1::2]  # Extracting the variable names
+    press_column_index = variables.index("Press")
+    Rho_column_index = variables.index("Rho  ")
+    MA_column_index = variables.index("Ma   ") 
+    T_column_index = variables.index("Ttrn ")
+    V_column_index = variables.index("Vx   ") 
+
+    # rhodif_index = variables.index("rho_dif") 
+    # padif_index = variables.index("Pa_dif") 
+
+    # Step 3: Preparing the modified second line to include "NEWVAR"
+    modified_second_line = variables_line.strip() + ',      "rho_dif",      "Ma_dif,      "Pa_dif",      "T_dif",      "V_ref", ,      "Rho_ref",      "P_ref",      "waveSpeed"'
+    # modified_second_line = variables_line.strip() + ',      "rho_dif",      "Ma_dif,      "Pa_dif",      "T_dif",      "V_ref", ,      "Rho_ref",      "P_ref",      "waveSpeed",      "rhoUNSTEADY",      "paUNSTEADY"'
+    modified.write(first_line + '\n')
+    modified.write(modified_second_line + '\n')
+    modified.write(third_line + '\n')
+    
+    # Iterate through the remaining lines
+    for line1, line2 in zip(mainFile, refFile):
+        stripped_lineMain = line1.strip()
+        stripped_lineRef = line2.strip()
+        # If line starts with "ZONE", copy it as-is
+        if stripped_lineMain.startswith("ZONE"):
+            modified.write(stripped_lineMain + '\n')
+        else:
+            # Split the line into individual values
+            mainValues = stripped_lineMain.split()
+            refValues = stripped_lineRef.split()
+            # ratio_pressure_percent = abs((float(mainValues[press_column_index]) - float(refValues[press_column_index])) / ((float(refValues[press_column_index]) + float(mainValues[press_column_index]) +1/100000000000)/2)*100)
+            ratio_pressure_percent = (float(mainValues[press_column_index]) - float(refValues[press_column_index]))
+            # ratio_pressure_percent = ((float(mainValues[press_column_index]) - float(refValues[press_column_index])) / ((float(refValues[press_column_index]) + float(mainValues[press_column_index]) +1/100000000000)/2)*100)
+            ratio_Rho_percent = ((float(mainValues[Rho_column_index]) - float(refValues[Rho_column_index])) / ((float(refValues[Rho_column_index]) + float(mainValues[Rho_column_index]) +1/100000000000)/2)*100)
+            ratio_Ma_percent = abs((float(mainValues[MA_column_index]) - float(refValues[MA_column_index])))
+            ratio_T_percent = abs((float(mainValues[T_column_index]) - float(refValues[T_column_index])) / ((float(refValues[T_column_index]) + float(mainValues[T_column_index]) +1/100000000000)/2)*100)
+            
+            #refs
+            V_ref = float(refValues[V_column_index])
+            rho_ref = float(refValues[Rho_column_index])
+            P_ref = float(refValues[press_column_index])
+            T_ref = float(refValues[T_column_index])
+            a = np.sqrt(298.8 * T_ref)
+            wavespeed = a + V_ref
+            # Add the value for "NEWVAR" column and join them back
+            # pa_unst = float(refValues[padif_index]) - float(refValues[padif_index])
+            # rho_unst = float(refValues[rhodif_index]) - float(refValues[padif_index])
+
+            rho_dif = 1
+            ma_dif = 2
+            modified_line = '\t'.join(mainValues + ["{:.6E}".format(ratio_Rho_percent)] + ["{:.6E}".format(ratio_Ma_percent)] + ["{:.6E}".format(ratio_pressure_percent)] + ["{:.6E}".format(ratio_T_percent)] + ["{:.6E}".format(V_ref)] + ["{:.6E}".format(rho_ref)] + ["{:.6E}".format(P_ref)] + ["{:.6E}".format(wavespeed)]) + '\n'
+            modified.write(modified_line)
+
+new_file_path
